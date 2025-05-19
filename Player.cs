9eb3ff6695a99a1404869374e9ef643fca13acf0
Saksham Kumar <174082750+saakshammm@users.noreply.github.com>
2025-05-19 12:32:28 +0530

    Update Player.cs

--- Player.cs
+++ Player.cs
@@ -19,30 +19,22 @@ public class Player : MonoBehaviour
     {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
-        transform.Translate(Vector3.right * horizontalInput * _speed * Time.deltaTime);
 
+        transform.Translate(Vector3.right * horizontalInput * _speed * Time.deltaTime);
         transform.Translate(Vector3.up * verticalInput * _speed * Time.deltaTime);
 
-        if(transform.position.y >= 0)
-        {
-            transform.position = new Vector3(transform.position.x, 0, 0);
-        }
-        
-        else if(transform.position.y <= -3.9f)
-        {
-            transform.position = new Vector3(transform.position.x, -3.9f, 0);
-        }
+      
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -3.9f, 0), 0);
 
-        if(transform.position.x <= -11.3f)
+        if (transform.position.x <= -11.3f)
         {
             transform.position = new Vector3(11.3f, transform.position.y, 0);
         }
 
-        else if(transform.position.x >= 11.3f)
+        else if (transform.position.x >= 11.3f)
         {
             transform.position = new Vector3(-11.3f, transform.position.y, 0);
         }
-
     }
 
 }
