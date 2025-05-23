f872fd3d9952db48ca3c1a3c7c0aee54b9d3bd28
Saksham Kumar <174082750+saakshammm@users.noreply.github.com>
2025-05-23 12:19:24 +0530

    Update Player.cs

--- Player.cs
+++ Player.cs
@@ -4,7 +4,13 @@ using System.Collections;
 public class Player : MonoBehaviour
 {
     [SerializeField]
-    public float _speed = 3.5f;
+    public float _speed = 5.3f;
+    [SerializeField]
+    private GameObject _lazerPrefab;
+    [SerializeField]
+    private float _fireRate = 0.1f;
+    [SerializeField]
+    private float _canFire = -1f;
 
     void Start()
     {
@@ -14,6 +20,12 @@ public class Player : MonoBehaviour
     void Update()
     {
         CalculateMovement();
+
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > _canFire)
+        {
+            _canFire = Time.time + _fireRate;
+            Instantiate(_lazerPrefab, transform.position + new Vector3(0, 0.8f, 0), Quaternion.identity);
+        }
     }
     void CalculateMovement()
     {
@@ -23,7 +35,7 @@ public class Player : MonoBehaviour
         transform.Translate(Vector3.right * horizontalInput * _speed * Time.deltaTime);
         transform.Translate(Vector3.up * verticalInput * _speed * Time.deltaTime);
 
-      
+
         transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -3.9f, 0), 0);
 
         if (transform.position.x <= -11.3f)
